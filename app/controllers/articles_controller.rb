28aae59c43147e95ad7d6001774e1dca43d705d8
Ahmed Ashraf <aabasha22@gmail.com>
2020-02-02 07:28:32 +0200

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "The article is deleted successfully!"
+    redirect_to articles_path
+  end
+
   def create
     #render plain: params[:article].inspect     #To display what is passed via params hash
     @article = Article.new(article_params)      #White list the params first
