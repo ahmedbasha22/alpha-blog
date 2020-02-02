fd64b9dc3b06991164270a43b391878cfbf0ff19
Ahmed Ashraf <aabasha22@gmail.com>
2020-02-02 06:45:45 +0200

    Add edit, update, show and articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,20 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def new
     #article deno
     @article = Article.new
-
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #render plain: params[:article].inspect     #To display what is passed via params hash
     @article = Article.new(article_params)      #White list the params first
-
     if @article.save
       flash[:notice] = "The article is created successfully!"     #Stays for one redirect cycle
       redirect_to article_path(@article)     #it needs the article (/id), Show action is needed
@@ -15,9 +22,21 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)      #Similar way to build an article
+      flash[:notice] = "The article is updated successfully!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
+
 
   private
     def article_params
